0b6a516dfa8c1bdad0299a137528cf9bad98991a
Paul <clerc.paul@hotmail.fr>
2018-05-04 17:42:59 +0200

    added subscribtion

--- imports/api/associations/server/publication.js
+++ imports/api/associations/server/publication.js
@@ -2,7 +2,9 @@ import {Meteor} from 'meteor/meteor'
 import {Associations} from '../associations'
 
 Meteor.publish('associations.all', function(){
-   
         return Associations.find({}, {limit: 100000, sort: {}})
+})
 
+Meteor.publish('associations.width_Image', function(){
+        return Associations.find({image_url: {$exists: true}}, {limit: 100000, sort: {}})
 })
--- imports/pages/AdminAssociations.js
+++ imports/pages/AdminAssociations.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 import { Grid, Button, Form, Container, Header, Image } from 'semantic-ui-react'
+import { withTracker } from 'meteor/react-meteor-data'
+import {Associations} from '/imports/api/associations/associations'
 
-export default class AdminAssociations extends Component {
+export class AdminAssociations extends Component {
     state = {
         association: {},
         form_message: ""
@@ -32,47 +34,70 @@ export default class AdminAssociations extends Component {
 
     render(){
         const {association, form_message, image_link} = this.state
-        return(
-            <Grid stackable>
-                <Grid.Column width={16}>
-                    <Container>
-                        <Header as='h1'>Gestion des associations</Header>
-                        <Form onSubmit={this.submit_form}>
-                            <Form.Input
-                                label='Nom'
-                                onChange={this.handleAssoChange}
-                                value={association.name}
-                                name='name'
-                                />
-                            <Form.Input
-                                label='Description'
-                                onChange={this.handleAssoChange}
-                                value={association.description}
-                                name='description'
-                                />
-                            <Form.Input
-                                label="URL de l'image"
-                                onChange={this.handleAssoChange}
-                                value={association.image_url}
-                                name='image_url'
-                                />
-                            <Form.Input
-                                label="URL de l'image link"
-                                onChange={this.handleChange}
-                                value={image_link}
-                                name='image_link'
-                                />
-                            <Button positive>Creer l'association </Button>
-                        </Form>
-                        <p>{form_message}</p>
-                        <p>{JSON.stringify(association)}</p>
-                        <p>{JSON.stringify(image_link)}</p>
-                        <Image size='small' src={association.image_url}/>
-                        <Image size='small' src={image_link}/>
+        const {loading, associations} = this.props
+        if (!loading) {
+            return(
+                <Grid stackable>
+                    <Grid.Column width={16}>
+                        <Container>
+                            <Header as='h1'>Gestion des associations</Header>
+                            <Form onSubmit={this.submit_form}>
+                                <Form.Input
+                                    label='Nom'
+                                    onChange={this.handleAssoChange}
+                                    value={association.name}
+                                    name='name'
+                                    />
+                                <Form.Input
+                                    label='Description'
+                                    onChange={this.handleAssoChange}
+                                    value={association.description}
+                                    name='description'
+                                    />
+                                <Form.Input
+                                    label="URL de l'image"
+                                    onChange={this.handleAssoChange}
+                                    value={association.image_url}
+                                    name='image_url'
+                                    />
+                                <Form.Input
+                                    label="URL de l'image link"
+                                    onChange={this.handleChange}
+                                    value={image_link}
+                                    name='image_link'
+                                    />
+                                <Button positive>Creer l'association </Button>
+                            </Form>
+                            <p>{form_message}</p>
+                            <p>{JSON.stringify(association)}</p>
+                            <p>{JSON.stringify(image_link)}</p>
+                            <Image size='small' src={association.image_url}/>
+                            <Image size='small' src={image_link}/>
+    
+                        </Container>
+                    </Grid.Column>
+                    {associations.length > 0 && 
+                        <Grid.Column width={16}>
+                            {associations.map( association => {
+                                return <p>{association.name}</p>
+                            })}
+                        </Grid.Column>
+                    }
+                </Grid>
+            )
 
-                    </Container>
-                </Grid.Column>
-            </Grid>
-        )
+        }else {
+            return <p>CA CHARGE PATIENTE UN PEU !!!</p>
+        }
     }
 }
+
+export default AdminAssociationsContainer = withTracker(() => {
+    const associationsPublication = Meteor.subscribe('associations.all')
+    const loading = !associationsPublication.ready()
+    const associations = Associations.find({}).fetch()
+    return {
+        loading,
+        associations
+    }
+})(AdminAssociations)
